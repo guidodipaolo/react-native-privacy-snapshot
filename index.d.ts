6a29a96013533eeed824f4cf66fe04819fdb7062
Radek Czemerys <7029942+radko93@users.noreply.github.com>
2020-07-23 11:16:28 +0200

    Add types

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+declare module 'react-native-privacy-snapshot' {
+  export funtion usePrivacySnapshot(): void;
+  export function show(): void;
+  export function hide(): void;
+  export function enabled(enabledFlag: boolean): void;
+}
